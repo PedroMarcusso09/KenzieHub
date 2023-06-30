aece1d4fd7df261bd0a3063eddf6aeb194251151
Pedro Henrique Abreu Marcusso <pedromarcusso@gmail.com>
2023-06-30 08:07:46 -0300

    fix: autologin

--- src/providers/TechContext.jsx
+++ src/providers/TechContext.jsx
@@ -10,55 +10,74 @@ const TechContext = createContext();
 const TechProvider = ({ children }) => {
   const [technologies, setTechnologies] = useState([]);
 
-  const token = localStorage.getItem('@TOKEN'); 
-  
+  const token = localStorage.getItem('@TOKEN');
+
   const fetchTechnologies = async () => {
-    const response = await api.get('/profile', {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    setTechnologies(response.data.techs);
+    try {
+      const response = await api.get('/profile', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setTechnologies(response.data.techs);
+    } catch (error) {
+      if (error.response && error.response.status === 401) {
+        // Tratar erro de autenticação, por exemplo, redirecionar para a página de login
+        console.log('Erro de autenticação');
+      } else {
+        console.log('Erro ao buscar tecnologias:', error.message);
+      }
+    }
   };
 
   useEffect(() => {
-    fetchTechnologies();
-  }, []);
+    if (token) {
+      fetchTechnologies();
+    }
+  }, [token]);
 
   const addTechnology = async (technology) => {
-    const token = localStorage.getItem('@TOKEN'); 
-    const response = await api.post('/users/techs', technology, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    setTechnologies([...technologies, response.data]);
+    try {
+      const response = await api.post('/users/techs', technology, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setTechnologies([...technologies, response.data]);
+    } catch (error) {
+      console.log('Erro ao adicionar tecnologia:', error.message);
+    }
   };
-  
+
   const updateTechnology = async (technologyId, status) => {
-    const token = localStorage.getItem('@TOKEN'); 
-    const response = await api.put(`/users/techs/${technologyId}`, { status }, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    setTechnologies(
-      technologies.map((tech) =>
-        tech.id === technologyId ? { ...tech, status: response.data.status } : tech
-      )
-    );
+    try {
+      const response = await api.put(`/users/techs/${technologyId}`, { status }, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setTechnologies(
+        technologies.map((tech) =>
+          tech.id === technologyId ? { ...tech, status: response.data.status } : tech
+        )
+      );
+    } catch (error) {
+      console.log('Erro ao atualizar tecnologia:', error.message);
+    }
   };
-  
+
   const deleteTechnology = async (technologyId) => {
-    const token = localStorage.getItem('@TOKEN');
-    await api.delete(`/users/techs/${technologyId}`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    setTechnologies(technologies.filter((tech) => tech.id !== technologyId));
+    try {
+      await api.delete(`/users/techs/${technologyId}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setTechnologies(technologies.filter((tech) => tech.id !== technologyId));
+    } catch (error) {
+      console.log('Erro ao excluir tecnologia:', error.message);
+    }
   };
-  
 
   return (
     <TechContext.Provider
